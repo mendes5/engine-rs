5d6cdaae4cc7cd68aba4270800f41798d656c08b
Fernando Mendes <fernando.mendes@tagview.com.br>
2021-02-05 13:24:03 -0300

    Deletes text-encoding

--- game/examples/javascript/modules/index.js
+++ game/examples/javascript/modules/index.js
@@ -1,17 +1,11 @@
 import { Vec2, Vec3 } from './vec.js';
 
 import Default from './subfolder/index.js';
-import { TextDecoder } from './text-encoding.js';
 
 import('./dynamic.js').then((x) => {
   Deno.core.print("Dynamic import resolved " + x.e);
   Deno.core.print("\n");
 });
-
-const decoder = new TextDecoder();
-
-Deno.core.print(decoder);
-Deno.core.print("\n");
 
 Deno.core.print(Default);
 Deno.core.print("\n");
